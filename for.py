2def846cea796d18c5fc1632d4c56fee820705f6
Andrew Brandt <brandt.andrew89@gmail.com>
2016-11-20 17:21:59 -0700

    Cleaned up for loop

--- for.py
+++ for.py
@@ -1,6 +1,12 @@
-x = 8
+# Basic example of implementing a for-loop
+
+# Create a variable called count to hold the current count
+count = 1
 print x
 
-for x in range(8, 12, 1):
-	x += 1
-	print x
+# For loop
+# for [variable] in range (start amount, stop amount, increment amount)
+for count in range(1, 12, 1):
+	# Add 1 to count and print the result
+	count = count + 1
+	print count
